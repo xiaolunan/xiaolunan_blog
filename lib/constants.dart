bc3ec9a626e1f611019ec0a6a72c9a6ea88bd9ee
RCL <18957806320@163.com>
2026-02-28 10:56:55 +0800

    登录

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   //家里电脑：192.168.0.138
   //公司电脑：192.168.2.13
-  static const String baseUrl = 'http://192.168.2.13:8080';
+  static const String baseUrl = 'http://192.168.2.45:8080';
   static const String captchaKey = '65463546453546754';
   static const int successCode = 20000; //操作成功
   static const int failureCode = 40000; //操作失败
